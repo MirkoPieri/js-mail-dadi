8248c491d0277531f92e4f1f3fa3661a1081d4da
MirkoPieri <mirko97.pieri@gmail.com>
2019-07-02 15:30:02 +0200

    Completamento Esercizio Email.

---
+++ Email/javascript.js
@@ -0,0 +1,24 @@
+// Chiedi all’utente la sua email,  controlla che sia nella lista di chi può accedere e stampa un messaggio appropriato;
+
+var email = ["user.us@gmail.com", "user998@gmail.com", "user@gmail.com", "user1234@gmail.com"];
+
+var emailUtente = prompt("Inserisci la tua email poi premi invio");
+
+var elemento = document.getElementById('mio_id');
+
+var emailCorretta;
+
+ // ciclo per vedere se l'email inserita è presente
+  for (var i = 0; i < email.length; i++) {
+    if (email[i] == emailUtente) {
+      emailCorretta = 1;
+    }
+  }
+
+// determinazione email presente o meno
+  if (emailCorretta == 1) {
+    elemento.innerHTML = "La tua email è corretta.";
+  } else {
+    alert("ATTENZIONE")
+    elemento.innerHTML = "La tua email non è presente nei nostri sistemi.";
+  }
